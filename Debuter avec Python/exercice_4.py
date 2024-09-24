dc08a0d3e37fe76d4b5e3d576baa7ce3fb0332fa
MaxenceTheG <maxenceg@lyceechurchill.london>
2024-09-24 14:14:23 +0000

    adding 2 functions

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,4 +1,23 @@
-c = 2
+#Transforming pseudocode into python
+c = 3
 P = 4*c
 A = c**2
 b = (A>5)
+
+print("A="+str(A)+" b="+str(b)+" P="+str(P))
+
+
+
+#Doing a perimetre function
+def perimetre(x):
+    return 4*x
+
+print(perimetre(c))
+
+
+
+#Doing an aire function
+def aire(x):
+    return x**2
+
+print(aire(c))
