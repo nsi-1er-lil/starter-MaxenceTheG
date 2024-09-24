62cd7983de5a557dec83b71a01b40821d02af551
MaxenceTheG <maxenceg@lyceechurchill.london>
2024-09-24 14:43:26 +0000

    committed

--- .github/workflows/exercice_4.py
+++ .github/workflows/exercice_4.py
@@ -1,1 +1,23 @@
+#Transforming pseudocode into python
+c = 3
+P = 4*c
+A = c**2
+b = (A>5)
 
+print("A="+str(A)+" b="+str(b)+" P="+str(P))
+
+
+
+#Doing a perimetre function
+def perimetre(x):
+    return 4*x
+
+print(perimetre(c))
+
+
+
+#Doing a surface function
+def surface(x):
+    return x**2
+
+print(surface(c))
